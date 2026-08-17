a1cd3d2b4d0f3b1e3602104313dd49ae9f3a0da8
GABRIEL RENATO NASCIMENTO MANZOLLA <25203680@dsn127m1242316.cfp127aluno.local>
2026-08-17 16:56:34 -0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,82 @@
+fun main() {
+   var idade: Int = 200
+    idade = 170
+
+    var altura = 1.80
+    altura = 1.76
+
+
+    val texto = "--soldier boy é muito bolado na série e buxa nos quadrinhos--"
+
+    /*
+    println(texto)
+
+    println(texto.length)
+    println(texto.uppercase())
+    println(texto.contains("human"))
+
+     */
+
+
+
+
+
+/*
+    val habilidade = "corte dimensional"
+    val renge = 20
+    val nome: String = "yami"
+
+    var dano = renge + (10-4)
+
+    print("A habilidade $habilidade do usuário $nome, casou ${dano*7}")
+
+ */
+
+
+
+/*
+    val texto = """
+        texto escreito
+        em varias linhas
+        para vermos q 
+        o anirap é um tamanduá (aniduá)
+    """.trimIndent()
+    print(texto)
+
+ */
+
+
+    val character = 's'
+
+    val blood: Boolean = false
+
+
+    val number = 7_000_000_000_000_000_000L
+    val peso = 75.123456789
+
+
+
+    val x: Int = 10
+    val y: Double = x.toDouble() * 12
+
+
+    println(y)
+}
+
+//k2 é uma merda
+
+/*
+soldier boy - anirap
+
+[...]
+pqp olha pros homens de hj em dia
+só b@iola afeminado
+no meu tempo 6 m@rri4n,
+
+n sei o q é bluthof
+n sei o q é internet
+pra co*er mu**er nunca precisei disso
+
+[...]
+*/
+
